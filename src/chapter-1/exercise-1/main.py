5998daf41037234cf4c0549e4ef20861cd00fef5
hollowdll <pixerchat@gmail.com>
2023-10-10 10:32:53 +0300

    chapter 1 exercises 1 and 2

---
+++ src/chapter-1/exercise-1/main.py
@@ -0,0 +1,8 @@
+# Sort characters
+
+def main():
+    word = input()
+    print(''.join(sorted(word)))
+
+if __name__ == "__main__":
+    main()
---
+++ src/chapter-1/exercise-2/main.py
@@ -0,0 +1,15 @@
+# Arithmetic
+
+def main():
+    a = int(input())
+    b = int(input())
+
+    print(f'{a} + {b} is {a + b}')
+    print(f'{a} - {b} is {a - b}')
+    print(f'{a} * {b} is {a * b}')
+    print(f'{a} / {b} is {a / b}')
+    print(f'{a} % {b} is {a % b}')
+    print(f'{a} ^ {b} is {a ** b}')
+
+if __name__ == "__main__":
+    main()
