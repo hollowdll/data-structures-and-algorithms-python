0bb34e5a4a7a29c0d3ffd21d735e67c5b4b3eaa3
hollowdll <pixerchat@gmail.com>
2023-10-24 09:20:04 +0300

    c1 e8 and e9

---
+++ src/chapter-1/exercise-9/main.py
@@ -0,0 +1,12 @@
+# Restaurant
+
+class Restaurant:
+    def __init__(self, name, cuisine_type):
+        self.name = name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f'{self.name} serves wonderful {self.cuisine_type}.')
+
+    def open_restaurant(self):
+        print(f'{self.name} is open. Come on in!')
