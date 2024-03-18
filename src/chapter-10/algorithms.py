ca123cef7a32742019a82358f25905bfd1332557
hollowdll <pixerchat@gmail.com>
2024-03-18 10:47:28 +0200

    c10 e3

--- src/chapter-10/algorithms.py
+++ src/chapter-10/algorithms.py
@@ -1,3 +1,5 @@
+from graph import Vertex, Edge, Graph
+
 def merge_sort(array):
     """
     Sort the array using the Merge sort algorithm
@@ -56,3 +58,45 @@ def fib(n):
         a, b = result, a
 
     return result
+
+def dijkstra_shortest_path(source_vertex: Vertex, destination_vertex: Vertex, graph: Graph):
+    """
+    Calculate the shortest past (in distance value) between given vertices
+    
+    Parameters:
+    - source_vertex: The source vertex
+    - destination_vertex: The destination vertex
+    - graph: The graph in question
+    
+    Returns: a tuple containing the minimum distance between vertices and a list of
+             vertices that form the minimum path from one vertex to the other.
+    """
+    unvisited_vertices = graph.get_vertices()
+    shortest_paths = { vertex: {'shortest': float('inf'), 'previous': None} for vertex in unvisited_vertices }
+    # source has distance 0
+    shortest_paths[source_vertex]['shortest'] = 0
+
+    while unvisited_vertices:
+        current_vertex = min(unvisited_vertices, key=lambda vertex: shortest_paths[vertex]['shortest'])
+        distance_from_source = shortest_paths[current_vertex]['shortest']
+        unvisited_adj_vertices = (v for v in graph.get_adjacent_vertices(current_vertex) if v in unvisited_vertices)
+
+        for adj_vertex in unvisited_adj_vertices:
+            edge = graph.get_edge(current_vertex, adj_vertex)
+            # distance from source to this vertex
+            total_distance = distance_from_source + edge.value()
+
+            if total_distance < shortest_paths[adj_vertex]['shortest']:
+                shortest_paths[adj_vertex]['shortest'] = total_distance
+                shortest_paths[adj_vertex]['previous'] = current_vertex
+
+        unvisited_vertices.remove(current_vertex)
+
+    path = []
+    current_vertex = destination_vertex
+
+    while current_vertex:
+        path.append(current_vertex)
+        current_vertex = shortest_paths[current_vertex]['previous']
+
+    return (shortest_paths[destination_vertex]['shortest'], path[::-1])
